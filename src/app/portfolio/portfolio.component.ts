ae8acd3704b8ae041d7b1548dbe3b4e472250b15
Stefan Maksimek <stfn_rl@MacBook-Pro-von-Stefan.fritz.box>
2022-10-10 11:13:12 +0200

    made dynamic parts

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
+  public projects = [
+    {
+      title: 'El Pollo Loco',
+      description:
+        'test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi adipisci deserunt nesciunt veniam minima eligendi, commodi cum numquam unde neque. test',
+      img: 'credential.png',
+      href: 'https://el-pollo-loco-e9eth27of-stefanmaksimek.vercel.app/',
+      status: 'ready',
+    },
+    {
+      title: 'Portfolio',
+      description:
+        'test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi adipisci deserunt nesciunt veniam minima eligendi, commodi cum numquam unde neque. test',
+      img: 'business_app.jpg',
+      href: '',
+      status: 'ready',
+    },
+    {
+      title: 'Kanban Project Management Tool',
+      description:
+        'test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi adipisci deserunt nesciunt veniam minima eligendi, commodi cum numquam unde neque. test',
+      img: 'laptop.jpg',
+      href: 'https://gruppe-272.developerakademie.net/Gruppenarbeit-Join-/index.html',
+      status: 'ready',
+    },
+    {
+      title: 'Poker Game',
+      description:
+        'test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi adipisci deserunt nesciunt veniam minima eligendi, commodi cum numquam unde neque. test',
+      img: 'ace.jpg',
+      href: 'https://el-pollo-loco-e9eth27of-stefanmaksimek.vercel.app/',
+      status: 'in progress',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  skills = [
+    { title: 'Angular', icon: 'angular.png' },
+    { title: 'JavaSript', icon: 'javascript.png' },
+    { title: 'Sass', icon: 'sass.png' },
+    { title: 'Git', icon: 'git.png' },
+    { title: 'Scrum', icon: 'scrum.png' },
+    { title: 'Design Thinking', icon: 'desing-thinking.png' },
+    { title: 'Databases', icon: 'databases.png' },
+    { title: 'Rest API', icon: 'rest-api.png' },
+    { title: 'Test automation', icon: 'test-automatisation.png' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
